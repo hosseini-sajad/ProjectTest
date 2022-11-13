f4071d7a29e03fe6f82d43a9f3918280b095a802
hosseini-sajad <hosseni_sajjad@yahoo.com>
2022-11-13 17:44:34 +0330

    Add mockito test

--- app/src/test/java/com/example/projecttest/FirstMockitoTest.kt
+++ app/src/test/java/com/example/projecttest/FirstMockitoTest.kt
@@ -1,8 +1,10 @@
 package com.example.projecttest
 
-import junit.framework.Assert.assertTrue
+import junit.framework.Assert.*
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
+import java.lang.RuntimeException
+
 
 class FirstMockitoTest {
 
@@ -12,4 +14,48 @@ class FirstMockitoTest {
     fun testIfCarIsACar() {
         assertTrue(myFerrari is Car)
     }
+
+    @Test
+    fun testDefaultBehaviourOfTestDouble() {
+        assertFalse(
+            "new test double should return false as boolean",
+            myFerrari.needsFuel()
+        );
+        assertEquals(
+            "new test double should return 0.0 as double",
+            0.0, myFerrari.getEngineTemperature()
+        );
+
+    }
+
+    @Test
+    fun testStubbing() {
+        assertFalse(
+            "new test double should return false as boolean",
+            myFerrari.needsFuel()
+        )
+
+        `when`(myFerrari.needsFuel()).thenReturn(true)
+
+        assertTrue(
+            "after instructed test double should return what we want",
+            myFerrari.needsFuel()
+        )
+    }
+
+    @Test(expected = RuntimeException::class)
+    fun throwException() {
+        `when`(myFerrari.needsFuel()).thenThrow(RuntimeException())
+        myFerrari.needsFuel()
+    }
+
+    @Test
+    fun testVerification() {
+        myFerrari.driveTo("Sweet home Shiraz")
+        myFerrari.needsFuel()
+
+        verify(myFerrari).driveTo("Sweet home Shiraz")
+        verify(myFerrari).needsFuel()
+    }
+
 }
